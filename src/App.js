46c2e424cab68456bebc4357d9a37db7ff521208
Pavan Srinivas <chinni.mamidala@gmail.com>
2021-11-28 01:18:49 -0500

    fetching tracking and map data

--- src/App.js
+++ src/App.js
@@ -2,36 +2,70 @@ import InputBar from "./components/InputBar/InputBar";
 import "mapbox-gl/dist/mapbox-gl.css";
 import Map from "./components/Map/Map";
 import SideBar from "./components/SideBar/SideBar";
-import React from "react";
-import useFetch from "react-fetch-hook";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import axios from "axios";
+
 
 function App() {
-  // eslint-disable-next-line no-unused-vars
-   const { isLoading, error, data} = useFetch("https://aviation-edge.com/v2/public/flights?key=c75eac-812e66&flightIata=FX77")
-    console.log(data);
+  const [flightTrackData, setFlightTrackData] = useState({});
+  const [cityData, setCityData] = useState({});
+  const [countryData,setCountryData] = useState({});  
+   
+  const fetchFlightData = async () => {
 
+    const flightdata = await axios.get(
+      "https://aviation-edge.com/v2/public/flights?key=c75eac-812e66&flightIata=AC91"
+    );
+      setFlightTrackData(flightdata);
+      console.log(flightTrackData)
+
+    const citydata = await axios.get(
+      `https://aviation-edge.com/v2/public/cityDatabase?key=c75eac-812e66&codeIataCity=${flightdata.data[0].departure.iataCode}`
+    )
+    setCityData(citydata);
+    console.log(cityData)
+
+    const countrydata = await axios.get(
+      ` https://aviation-edge.com/v2/public/countryDatabase?key=c75eac-812e66&codeIso2Country=${citydata.data[0].codeIso2Country} `
+    )
+    setCountryData(countrydata);
+    console.log(countryData)
+
+  };                                                  
   
+ 
 
-   return (
+  useEffect(() => {
+     fetchFlightData();
+  },[]);
+
+
+    return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LinkForFlightTracker />}></Route>
-          <Route path="/flight-tracker" element={<FlightTracker data={data} />}></Route>
+          <Route
+            path="/flight-tracker"
+            element={<FlightTracker data={flightTrackData,cityData,countryData} />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
-function FlightTracker(data) {
+function FlightTracker(flightTrackData,cityData,countryData) {
   return (
+    (flightTrackData && cityData && countryData) ? 
     <div>
       <InputBar />
-      <Map props={data} />
-      <SideBar props={data}/>
+      <Map props={flightTrackData,cityData} />
+      <SideBar props={flightTrackData,cityData,countryData} />
     </div>
+    :
+    <div>Loading...</div>
   );
 }
 
